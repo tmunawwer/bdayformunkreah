d76b4b9626c05ab09e41381e23d94f6b4e70e5a2
tmunawwer <37419565+tmunawwer@users.noreply.github.com>
2018-06-14 17:31:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    //console.log('I am ready!');
     client.user.setStatus("Online");
     client.user.setActivity("Munkreah's Birthday");
 });
