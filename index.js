6e3c48ead5470df26a2792a95138411009e88253
tmunawwer <37419565+tmunawwer@users.noreply.github.com>
2018-06-14 17:18:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.on('ready', () =>{
-    bot.user.setStatus("Online")
-    bot.user.setActivity("testing")
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-bot.on('message', (message) => {
-  if(message.content == 'test'){
-    message.channel.send("tested");
-  }
-}
-bot.login('NDU2OTIzNjQ2MDQxMjYwMDQy.DgRnnQ.d10C0MiX8R3VKU8ejXUifHesM9k');
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login('NDU2OTIzNjQ2MDQxMjYwMDQy.DgRnnQ.d10C0MiX8R3VKU8ejXUifHesM9k');
