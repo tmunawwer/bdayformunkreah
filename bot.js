cb3f15829d8b5f18b78e5b3a232ed6e10fb0ea44
tmunawwer <37419565+tmunawwer@users.noreply.github.com>
2018-06-22 12:04:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,26 +21,25 @@ client.on('message', message => {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
+    title: "COMMANDS:",
+    description: "Birthday Bot for munkreah",
     fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
+        name: "Birthday Song",
+        value: "type \"Start\" to begin the song.\ntype \"Stop\" to stop the song."
       },
       {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
+        name: "Gifts",
+        value: "type \"Gifts\" to see this command"
       },
       {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
+        name: "Random",
+        value: ":V <- V: and so on"
       }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Example"
+      text: "Birthday Bot"
     }
   }
 });
